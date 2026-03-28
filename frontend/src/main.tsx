baa009ca8eb14de5fdbfa56faef898c8d177f977
Yasu Fadhili <yasufad@proton.me>
2026-03-28 18:08:55 +0300

    feat(frontend): wrap app root with FluentProvider for Fluent UI theming

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,13 @@
+import {
+  FluentProvider,
+  webLightTheme,
+  webDarkTheme,
+} from "@fluentui/react-components";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <FluentProvider theme={}></FluentProvider>
+  </React.StrictMode>,
 );
