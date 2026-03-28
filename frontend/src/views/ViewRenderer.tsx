2ab4ebcf403c51c2760961e9d832140f8a788098
Yasu Fadhili <yasufad@proton.me>
2026-03-28 19:12:26 +0300

    refactor(frontend): update ViewRenderer to import and use actual view components

--- frontend/src/views/ViewRenderer.tsx
+++ frontend/src/views/ViewRenderer.tsx
@@ -1,33 +1,31 @@
 import { useNavigationStore } from "../store/navigation";
+import { DashboardView } from "./DashboardView";
+import { ConnectionsView } from "./ConnectionsView";
+import { HostsView } from "./HostsView";
+import { ProtocolsView } from "./ProtocolsView";
+import { AlertsView } from "./AlertsView";
+import { SessionsView } from "./SessionsView";
 import { SettingsView } from "./SettingsView";
-
-// Placeholder components for each view
-const Dashboard = () => <div>Dashboard View Loading...</div>;
-const Connections = () => <div>Connections View Loading...</div>;
-const Hosts = () => <div>Hosts View Loading...</div>;
-const Protocols = () => <div>Protocols View Loading...</div>;
-const Alerts = () => <div>Alerts View Loading...</div>;
-const Sessions = () => <div>Sessions View Loading...</div>;
 
 export const ViewRenderer = () => {
   const activeView = useNavigationStore((state) => state.activeView);
 
   switch (activeView) {
     case "dashboard":
-      return <Dashboard />;
+      return <DashboardView />;
     case "connections":
-      return <Connections />;
+      return <ConnectionsView />;
     case "hosts":
-      return <Hosts />;
+      return <HostsView />;
     case "protocols":
-      return <Protocols />;
+      return <ProtocolsView />;
     case "alerts":
-      return <Alerts />;
+      return <AlertsView />;
     case "sessions":
-      return <Sessions />;
+      return <SessionsView />;
     case "settings":
       return <SettingsView />;
     default:
-      return <Dashboard />;
+      return <DashboardView />;
   }
 };
